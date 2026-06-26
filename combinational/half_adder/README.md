6c9b038f19e80417f159da0df268fafd2c5b1b75
Abinanth Pauly <abinanthpauly2003@gmail.com>
2026-06-26 12:46:18 +0530

    Reorganize repository and add combinational circuits

---
+++ combinational/half_adder/README.md
@@ -0,0 +1,15 @@
+# Half Adder
+
+## Logic
+
+SUM = A XOR B
+
+CARRY = A AND B
+
+## Truth Table
+
+A B | SUM CARRY
+0 0 | 0   0
+0 1 | 1   0
+1 0 | 1   0
+1 1 | 0   1
---
+++ combinational/half_adder/tb_half_adder.v
@@ -0,0 +1,28 @@
+module tb_half_adder;
+
+    reg a, b;
+    wire sum, carry;
+
+    half_adder uut (
+        .a(a),
+        .b(b),
+        .sum(sum),
+        .carry(carry)
+    );
+
+    initial begin
+        // Waveform generation
+        $dumpfile("wave.vcd");
+        $dumpvars(0, tb_half_adder);
+
+        $display("a b | sum carry");
+        $monitor("%b %b |  %b    %b", a, b, sum, carry);
+
+        a = 0; b = 0;
+        #10 a = 0; b = 1;
+        #10 a = 1; b = 0;
+        #10 a = 1; b = 1;
+        #10 $finish;
+    end
+
+endmodule
